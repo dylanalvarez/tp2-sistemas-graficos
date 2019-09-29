91d7d7709545b6bace7329eff654bef4d93a0549
dylan <dylanalvarez1995@gmail.com>
2019-09-29 16:04:38 -0300

    Make the car look kind of like an actual car

--- src/js/models/car.js
+++ src/js/models/car.js
@@ -4,11 +4,32 @@ import Cylinder from './cylinder'
 
 export default class Car extends TreeNode {
     draw(modelMatrix, viewMatrix, projMatrix) {
-        new Cylinder().draw(modelMatrix, viewMatrix, projMatrix);
-
         let childModelMatrix = mat4.clone(modelMatrix);
-        mat4.translate(childModelMatrix, childModelMatrix, [0, 0, 2]);
         mat4.rotate(childModelMatrix, childModelMatrix, Math.PI / 2, [1, 0, 0])
+        mat4.scale(childModelMatrix, childModelMatrix, [0.5, 1, 2]);
+        new Cylinder().draw(childModelMatrix, viewMatrix, projMatrix);
+
+        childModelMatrix = mat4.clone(modelMatrix);
+        mat4.translate(childModelMatrix, childModelMatrix, [0, 1.2, 2]);
+        mat4.rotate(childModelMatrix, childModelMatrix, Math.PI / 2, [0, 0, 1])
+        mat4.scale(childModelMatrix, childModelMatrix, [0.3, 0.3, 0.3]);
+        new Cylinder().draw(childModelMatrix, viewMatrix, projMatrix);
+
+        childModelMatrix = mat4.clone(modelMatrix);
+        mat4.translate(childModelMatrix, childModelMatrix, [0, -1.2, 2]);
+        mat4.rotate(childModelMatrix, childModelMatrix, Math.PI / 2, [0, 0, 1])
+        mat4.scale(childModelMatrix, childModelMatrix, [0.3, 0.3, 0.3]);
+        new Cylinder().draw(childModelMatrix, viewMatrix, projMatrix);
+
+        childModelMatrix = mat4.clone(modelMatrix);
+        mat4.translate(childModelMatrix, childModelMatrix, [0, 1.2, -2]);
+        mat4.rotate(childModelMatrix, childModelMatrix, Math.PI / 2, [0, 0, 1])
+        mat4.scale(childModelMatrix, childModelMatrix, [0.3, 0.3, 0.3]);
+        new Cylinder().draw(childModelMatrix, viewMatrix, projMatrix);
+
+        childModelMatrix = mat4.clone(modelMatrix);
+        mat4.translate(childModelMatrix, childModelMatrix, [0, -1.2, -2]);
+        mat4.rotate(childModelMatrix, childModelMatrix, Math.PI / 2, [0, 0, 1])
         mat4.scale(childModelMatrix, childModelMatrix, [0.3, 0.3, 0.3]);
         new Cylinder().draw(childModelMatrix, viewMatrix, projMatrix);
     }

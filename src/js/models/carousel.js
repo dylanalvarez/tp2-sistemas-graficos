702d6ce20c6d37d682849ac2f11c5a96d4a7399b
dylan <dylanalvarez1995@gmail.com>
2019-10-06 23:28:28 -0300

    Draw basic carousel

--- src/js/models/carousel.js
+++ src/js/models/carousel.js
@@ -5,11 +5,20 @@ import Cylinder from './cylinder'
 export default class Carousel extends TreeNode {
     constructor() {
         super();
-        this.center = new Cylinder();
+        this.base = new Cylinder();
+        this.top = new Cylinder();
     }
 
     draw(modelMatrix, viewMatrix, projMatrix) {
-        let childModelMatrix = mat4.clone(modelMatrix);
-        this.center.draw(childModelMatrix, viewMatrix, projMatrix);
+        let baseModelMatrix = mat4.clone(modelMatrix);
+        mat4.rotate(baseModelMatrix, baseModelMatrix, Math.PI / 2, [1, 0, 0]);
+        mat4.scale(baseModelMatrix, baseModelMatrix, [0.1, 0.1, 1]);
+        this.base.draw(baseModelMatrix, viewMatrix, projMatrix);
+
+        let topModelMatrix = mat4.clone(modelMatrix);
+        mat4.translate(topModelMatrix, topModelMatrix, [0, -1, 0]);
+        mat4.rotate(topModelMatrix, topModelMatrix, Math.PI / 2, [1, 0, 0]);
+        mat4.scale(topModelMatrix, topModelMatrix, [1, 1, 0.1]);
+        this.top.draw(topModelMatrix, viewMatrix, projMatrix);
     }
 }

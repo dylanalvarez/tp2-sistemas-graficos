303aba8675afedc1d42f2b705edd17005c3ac10f
FScaccheri <francocass2009@hotmail.com>
2019-10-06 23:41:13 -0300

    Changed shaders to show flat colors

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -9,5 +9,5 @@ void main(void) {
 	vec3 diffColor=mix(vec3(0.7,0.7,0.7),uColor,0.9);						// color del pixel
 	vec3 colorFinal=dot(lightVec,vNormal)*diffColor+vec3(0.2,0.2,0.2);		// color final iluminado
 
-   gl_FragColor = vec4(colorFinal, 1.0);
+   gl_FragColor = vec4(uColor, 1.0);
 }

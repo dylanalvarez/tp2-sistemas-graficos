17602eee2beba443709601d9b73bb9fea29977e1
Franco Scaccheri Cassanello <fcassanello@devartis.com>
2019-10-08 14:10:16 -0300

    Defined more color constans

--- src/js/colors.js
+++ src/js/colors.js
@@ -2,5 +2,12 @@ export default {
     black: [0, 0, 0, 1],
     grassGreen: [0.26, 0.82, 0.27, 1.0],
     skyBlue: [0.32, 0.53, 0.87, 1.0],
-    white: [1, 1, 1, 1]
+    white: [1, 1, 1, 1],
+    carYellow: [0.98, 0.76, 0.01, 1.0],
+    carouselTopOrange: [0.99, 0.62, 0.04, 1.0],
+    carouselChairMagenta: [0.87, 0.14, 0.96, 1.0],
+    carChairGreen: [0.33, 0.46, 0.02, 1.0],
+    carouselBaseBlue: [0.14, 0.54, 0.85, 1.0],
+    rollerCoasterGrey: [0.57, 0.56, 0.55, 1.0],
+    pillarRed: [0.83, 0.3, 0.21, 1.0]
 }

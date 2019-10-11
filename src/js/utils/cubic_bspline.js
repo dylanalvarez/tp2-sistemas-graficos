7256a8e6951ce33d687662f7eda647c3c3752b74
FScaccheri <francocass2009@hotmail.com>
2019-10-10 21:41:55 -0300

    Created utility class for cubic B-Spline curve creation

---
+++ src/js/utils/cubic_bspline.js
@@ -0,0 +1,60 @@
+export default class BSpline {
+    constructor(controlPoints) {
+        // Vector de 4 puntos de control que forman la curva cubica B-Spline
+        // Cada punto de control es otro vector con las coordenadas [x,y,z]
+        this.controlPoints = controlPoints;
+    }
+
+    base0(u) {
+        return (1-u)*(1-u)*(1-u)*1/6;       // 1/6*(1-u)^3 
+    }
+
+    base1(u) {
+        return (4-6*u*u+3*u*u*u)*1/6;       // (u^3)/2 - u +2/3
+    }
+
+    base2(u) {
+        return (1+3*u+3*u*u-3*u*u*u)*1/6;   // -0.5*u^3+0.5*u^2+0.5*u+1
+    }
+
+    base3(u) {
+        return (u*u*u)*1/6;                 // 1/6*u^3
+    }
+
+    derivativeBase0(u) {
+        return -0.5*Math.pow(1-u,2);        // -((1-u)^2)/2
+    }
+    derivativeBase1(u) {
+        return (3/2)*u*u-2*u;
+    }
+    derivativeBase2(u) {
+        return -1.5*u*u+u+0.5;
+     }
+    derivativeBase3(u) {
+        return (u*u)/2;
+    }
+
+    BSplineCurve(u) {
+        // Renombro atributos y funciones para emprolijar la ecuacion
+        b0 = this.base0; b1 = this.base1; b2 = this.base2; b3 = this.base3;
+        p0 = this.controlPoints[0]; p1 = this.controlPoints[1]; p2 = this.controlPoints[2]; p3 = this.controlPoints[3];
+        
+        x = b0(u) * p0[0] + b1(u) * p1[0] + b2(u) * p2[0] + b3(u) * p3[0];
+        y = b0(u) * p0[1] + b1(u) * p1[1] + b2(u) * p2[1] + b3(u) * p3[1];
+        z = b0(u) * p0[2] + b1(u) * p1[2] + b2(u) * p2[2] + b3(u) * p3[2];
+
+        return [x, y, z];
+    }
+
+    BSplineDerivativeCurve(u) {
+        // Renombro atributos y funciones para emprolijar la ecuacion
+        db0 = this.derivativeBase0; db1 = this.derivativeBase1; db2 = this.derivativeBase2; db3 = this.derivativeBase3;
+        p0 = this.controlPoints[0]; p1 = this.controlPoints[1]; p2 = this.controlPoints[2]; p3 = this.controlPoints[3];
+        
+        x = db0(u) * p0[0] + db1(u) * p1[0] + db2(u) * p2[0] + db3(u) * p3[0];
+        y = db0(u) * p0[1] + db1(u) * p1[1] + db2(u) * p2[1] + db3(u) * p3[1];
+        z = db0(u) * p0[2] + db1(u) * p1[2] + db2(u) * p2[2] + db3(u) * p3[2];
+
+        return [x, y, z];
+    }
+}

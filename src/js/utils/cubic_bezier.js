ef4a0ee53ad037a62107fff08adf0900a26f8874
FScaccheri <francocass2009@hotmail.com>
2019-10-17 23:13:22 -0300

    Removed comments

--- src/js/utils/cubic_bezier.js
+++ src/js/utils/cubic_bezier.js
@@ -3,29 +3,29 @@ export default class Bezier {
         this.controlPoints = controlPoints;
     }
 
-    base0(u) { return (1-u)*(1-u)*(1-u); }  // 1*(1-u) - u*(1-u) = 1-2u+u2  ,  (1-2u+u2) - u +2u2- u3 ,  1 - 3u +3u2 -u3
+    base0(u) { return (1-u)*(1-u)*(1-u); }
 
     base1(u) { return 3*(1-u)*(1-u)*u; }  
     
-	base2(u) { return 3*(1-u)*u*u; }     //   -u3 +3u2
+	base2(u) { return 3*(1-u)*u*u; }
     
-	base3(u) {  return u*u*u; }		 // u3
+	base3(u) {  return u*u*u; }
 
-    derivativeBase0(u) { return -3*u*u+6*u-3;} 	//-3u2 +6u -3
+    derivativeBase0(u) { return -3*u*u+6*u-3;}
 
-	derivativeBase1(u) { return 9*u*u-12*u+3; }  // 9u2 -12u +3
+	derivativeBase1(u) { return 9*u*u-12*u+3; }
 
-	derivativeBase2(u) { return -9*u*u+6*u;}		 // -9u2 +6u
+	derivativeBase2(u) { return -9*u*u+6*u;}
 
-	derivativeBase3(u) { return 3*u*u; }			// 3u2
+	derivativeBase3(u) { return 3*u*u; }
 
-	doubleDerivativeBase0(u) { return -6*u+6;} 		//-6u +6
+	doubleDerivativeBase0(u) { return -6*u+6;}
 
-	doubleDerivativeBase1(u) { return 18*u-12; }  		// 18u-12 
+	doubleDerivativeBase1(u) { return 18*u-12; }
 
-	doubleDerivativeBase2(u) { return 6-18*u;}		 // -18u +6
+	doubleDerivativeBase2(u) { return 6-18*u; }
 
-    doubleDerivativeBase3(u) { return 6*u; }			// 6u
+    doubleDerivativeBase3(u) { return 6*u; }
 
     bezierCurve(u) {
         let b0=this.base0, b1=this.base1, b2=this.base2, b3=this.base3;

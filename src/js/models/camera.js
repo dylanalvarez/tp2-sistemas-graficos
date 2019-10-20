d7de62e310efd2b1352a7a248e520f4ca251b763
dylan <dylanalvarez1995@gmail.com>
2019-10-20 14:18:20 -0300

    Implement orbital cameras, fine-tune default variables for each one

--- src/js/models/camera.js
+++ src/js/models/camera.js
@@ -2,9 +2,7 @@ import { mat4, vec4 } from 'gl-matrix'
 
 export default class Camera {
     constructor(canvas) {
-        this.step = 0.2;
         this.angleMultiplier = Math.PI / 500;
-        this.yAngleLimit =  (3 / 8) * Math.PI;
         this.pressedKeys = new Set();
         this.mode = 'first_person';
         this.setDefaults();
@@ -33,11 +31,13 @@ export default class Camera {
             this.setDefaults();
         }
         canvas.onmousedown = (event) => {
+            if (event.which !== 1) return;
             this.listenToMouse = true;
             this.lastMouseX = event.x;
             this.lastMouseY = event.y;
         }
         canvas.onmouseup = () => {
+            if (event.which !== 1) return;
             this.listenToMouse = false;
         }
         canvas.onmousemove = (event) => {
@@ -52,30 +52,58 @@ export default class Camera {
     }
 
     setDefaults() {
+        let cameraModeNameDiv = document.getElementById('camera-mode-name');
         if (this.mode === 'first_person') {
+            cameraModeNameDiv.innerHTML = '<b>Primera persona</b><br>(<b>C</b> cambia cámara, navegar con <b>mouse</b>, <b>WASD</b> y <b>QE</b>)'
+            this.yAngleUpperLimit =  (3 / 8) * Math.PI;
+            this.yAngleLowerLimit =  - (3 / 8) * Math.PI;    
             this.offsetX = 0;
-            this.offsetY = 2;
-            this.offsetZ = 0;
+            this.offsetY = 6;
+            this.offsetZ = 2;
             this.xAngle = -0.37;
-            this.yAngle = 0.05;
+            this.yAngle = -0.3;
+            this.step = 0.3;
         } else if (this.mode === 'general') {
-            this.offsetX = -5.4;
-            this.offsetY = 0.6;
-            this.offsetZ = -24.3;
-            this.xAngle = -2.04;
-            this.yAngle = 0.23;
+            cameraModeNameDiv.innerHTML = '<b>Vista orbital general</b><br>(<b>C</b> cambia cámara, navegar con <b>mouse</b> y <b>WS</b>)'
+            this.yAngleUpperLimit =  (1 / 64) * Math.PI;
+            this.yAngleLowerLimit =  - (3 / 8) * Math.PI;    
+            this.orbitalRadius = 40;
+            this.maxOrbitalRadius = 60;
+            this.minOrbitalRadius = 20;
+            this.offsetX = 5;
+            this.offsetY = 3;
+            this.offsetZ = -24;
+            this.xAngle = 0.6;
+            this.yAngle = -0.3;
+            this.step = 0.4;
         } else if (this.mode === 'rollercoaster') {
-            this.offsetX = 13.5;
-            this.offsetY = 10.4;
-            this.offsetZ = -20.9;
-            this.xAngle = 0.08;
-            this.yAngle = -0.39;
+            cameraModeNameDiv.innerHTML = '<b>Vista orbital montaña rusa</b><br>(<b>C</b> cambia cámara, navegar con <b>mouse</b> y <b>WS</b>)'
+            this.yAngleUpperLimit = (1 / 64) * Math.PI;
+            this.yAngleLowerLimit = - (3 / 8) * Math.PI;
+            this.orbitalRadius = 28;
+            this.maxOrbitalRadius = 40;
+            this.minOrbitalRadius = 10;
+            this.offsetX = 12;
+            this.offsetY = 5;
+            this.offsetZ = -28;
+            this.xAngle = -0.9;
+            this.yAngle = -0.3;
+            this.step = 0.3;
         } else if (this.mode === 'chairs') {
-            this.offsetX = 1;
-            this.offsetY = 1;
-            this.offsetZ = -8.6;
-            this.xAngle = 1.23;
-            this.yAngle = 0.08;
+            cameraModeNameDiv.innerHTML = '<b>Vista orbital sillas voladoras</b><br>(<b>C</b> cambia cámara, navegar con <b>mouse</b> y <b>WS</b>)'
+            this.yAngleUpperLimit = (1 / 64) * Math.PI;
+            this.yAngleLowerLimit = - (3 / 8) * Math.PI;
+            this.orbitalRadius = 7;
+            this.maxOrbitalRadius = 10;
+            this.minOrbitalRadius = 3;
+            this.offsetX = -2.9;
+            this.offsetY = 0.7;
+            this.offsetZ = -10;
+            this.xAngle = -1;
+            this.yAngle = -0.1;
+            this.step = 0.1;
+        } else {
+            cameraModeNameDiv.innerHTML = '<b>Vista desde carro</b><br>(<b>C</b> cambia cámara)'
         }
     }
 
@@ -100,12 +128,18 @@ export default class Camera {
         let difference = (this.lastMouseY - mouseY) * this.angleMultiplier;
         let angle = (this.yAngle + difference) % (Math.PI * 2);
         this.yAngle = angle;
-        if (angle > this.yAngleLimit) {
-            this.yAngle = this.yAngleLimit;
+        if (angle > this.yAngleUpperLimit) {
+            this.yAngle = this.yAngleUpperLimit;
         }
-        if (angle < -this.yAngleLimit) {
-            this.yAngle = -this.yAngleLimit;
+        if (angle < this.yAngleLowerLimit) {
+            this.yAngle = this.yAngleLowerLimit;
         }
+    }
+
+    updateOrbitalRadius() {
+        this.orbitalRadius += this.offset('W', 'S') * this.step;
+        if (this.orbitalRadius < this.minOrbitalRadius) this.orbitalRadius = this.minOrbitalRadius;
+        if (this.orbitalRadius > this.maxOrbitalRadius) this.orbitalRadius = this.maxOrbitalRadius;
     }
 
     updateOffsets() {
@@ -132,13 +166,13 @@ export default class Camera {
         }
 
         this.offsetX += x * this.step;
-        this.offsetY = Math.max(this.offsetY + this.offset('E', 'Q') * this.step, 0.1);
+        this.offsetY = Math.max(this.offsetY + this.offset('E', 'Q') * this.step, 0.2);
         this.offsetZ += z * this.step;
     }
 
     updateCarPosition(position) {
         mat4.rotateX(position, position, Math.PI / 2);
-        mat4.translate(position, position, [2, 0, 0]);
+        mat4.translate(position, position, [3, 0, 1]);
         this.carPosition = position;
     }
 
@@ -160,7 +194,11 @@ export default class Camera {
             return;
         }
         this.eye = mat4.create();
-        if (this.mode === 'first_person') this.updateOffsets();
+        if (this.mode === 'first_person') {
+            this.updateOffsets();
+        } else {
+            this.updateOrbitalRadius();
+        }
         this.applyOffsets(this.eye);
         mat4.rotateY(this.eye, this.eye, this.xAngle);
         mat4.rotateX(this.eye, this.eye, this.yAngle);
@@ -178,11 +216,28 @@ export default class Camera {
             eyePosition[2] - eyeTangent[2],
         ]
 
-        mat4.lookAt(
-            viewMatrix,
-            eyePosition,
-            centerPosition,
-            [0, 1, 0]
-        )
+        if (this.mode === 'first_person' || this.mode === 'car') {
+            mat4.lookAt(
+                viewMatrix,
+                eyePosition,
+                centerPosition,
+                [0, 1, 0]
+            )
+            console.log(eyePosition);
+        } else {
+            let eye = mat4.create();
+
+            mat4.translate(eye, eye, [this.offsetX, this.offsetY, this.offsetZ]);
+            mat4.rotateY(eye, eye, this.xAngle);
+            mat4.rotateX(eye, eye, this.yAngle);
+            mat4.translate(eye, eye, [0, 0, this.orbitalRadius]);
+
+            mat4.lookAt(
+                viewMatrix,
+                this.position(eye),
+                [this.offsetX, this.offsetY, this.offsetZ],
+                [0, 1, 0]
+            )    
+        }
     }
 }

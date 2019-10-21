e1684c65d2f34b27518cf229bf361783e36fbb68
dylan <dylanalvarez1995@gmail.com>
2019-10-21 16:42:40 -0300

    Tweak saturation in fragment shader

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -8,7 +8,7 @@ void main(void) {
 	vec3 lightVec=normalize(vec3(0.0, 90.0, 0.0)-vPosWorld);
 	vec3 diffColor=uColor;
 	vec3 colorFinal=dot(lightVec,vNormal)*diffColor+vec3(0.2,0.2,0.2);
-	colorFinal= mix(uColor, colorFinal, 0.5);
+	colorFinal= mix(uColor, colorFinal, 0.3);
 
     gl_FragColor = vec4(colorFinal, 1.0);
 }

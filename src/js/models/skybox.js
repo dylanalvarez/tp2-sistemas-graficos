d608ae62282f1f6957f1b12a7e500d26a8c0d01f
FScaccheri <francocass2009@hotmail.com>
2019-11-10 23:05:26 -0300

    Linter'd

--- src/js/models/skybox.js
+++ src/js/models/skybox.js
@@ -10,23 +10,23 @@ export default class Skybox extends Sphere {
     initTexture(filePath) {
         this.texture = gl.createTexture();
         this.texture.image = new Image();
-        
+
         let texture = this.texture;
-		this.texture.image.onload = function () {
-            	
+        this.texture.image.onload = function () {
+
             gl.bindTexture(gl.TEXTURE_2D, texture); 						// activo la textura
-                        
+
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);	// cargo el bitmap en la GPU
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);					// selecciono filtro de magnificacion
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);	// selecciono filtro de minificacion
-                        
+
             gl.generateMipmap(gl.TEXTURE_2D);		// genero los mipmaps
             gl.bindTexture(gl.TEXTURE_2D, null);
         }
         this.texture.image.src = filePath;
-            
+
     }
-    
+
     buildBuffers() {
         let pos = [];
         let normal = [];
@@ -38,11 +38,11 @@ export default class Skybox extends Sphere {
         this.initTexture(sunsetImage);
 
         for (let i = 0; i < rows; i++) {
-            for (let j=0; j < cols; j++) {
+            for (let j = 0; j < cols; j++) {
                 let alphaStep = j / (cols - 1);
                 let betaStep = i / (rows - 1);
 
-                let alpha = alphaStep * Math.PI * 2 ;
+                let alpha = alphaStep * Math.PI * 2;
                 let beta = -betaStep * Math.PI;
 
                 let p = this.getPosInSphere(alpha, beta, radius);
@@ -51,7 +51,7 @@ export default class Skybox extends Sphere {
                 pos.push(p[2]);
 
                 let n = this.getNrm(alpha, beta, radius);
-                
+
                 normal.push(-n[0]);
                 normal.push(-n[1]);
                 normal.push(-n[2]);
@@ -74,7 +74,7 @@ export default class Skybox extends Sphere {
         let trianglesNormalBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, trianglesNormalBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normal), gl.STATIC_DRAW);
-        
+
         let trianglesUvBuffer = gl.createBuffer();
         trianglesUvBuffer.number_point = uv.length;
         gl.bindBuffer(gl.ARRAY_BUFFER, trianglesUvBuffer);

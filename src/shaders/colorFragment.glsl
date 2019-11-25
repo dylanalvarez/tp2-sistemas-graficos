fb02248dfb755573bf8a073e6b03b1ace8be2ba0
FScaccheri <francocass2009@hotmail.com>
2019-11-24 23:56:07 -0300

    Renamed phong reflection shader function. Limited diffused and specular terms to values between 0 and 1

--- src/shaders/colorFragment.glsl
+++ src/shaders/colorFragment.glsl
@@ -11,26 +11,26 @@ uniform vec3 uLightSix;
 
 uniform vec3 uViewerPosition;
 
-vec3 phongIlluminationVector() {
+vec3 phongReflection() {
 	vec3 lightVec = normalize(vec3(0.0, 60.0, 0.0)-vPosWorld);
 	
 	// Iluminacion ambiental de Phong
-	vec3 ka = vec3(0.5, 0.5, 0.5); // Valor constante por ahora, cambiara al definir materiales
-	vec3 ia = vec3(1.0, 1.0, 1.0); // Intensidad de iluminacion ambiente
+	vec3 ka = vec3(1.0, 1.0, 1.0); // Valor constante por ahora, cambiara al definir materiales
+	vec3 ia = vec3(0.6, 0.6, 0.6); // Intensidad de iluminacion ambiente
 	vec3 ambientIllumination = ka*ia;
 
 	// Iluminacion difusa de Phong
-	vec3 kd = vec3(0.25, 0.25, 0.25); // Valor constante por ahora, cambiara al definir materiales
-	vec3 id = vec3(1.0, 1.0, 1.0); // Intensidad de iluminacion difusa
-	vec3 diffuseIllumination = dot(lightVec, vNormal)*kd*id;
+	vec3 kd = vec3(1.0, 1.0, 1.0); // Valor constante por ahora, cambiara al definir materiales
+	vec3 id = vec3(0.2, 0.2, 0.2); // Intensidad de iluminacion difusa
+	vec3 diffuseIllumination = clamp(dot(lightVec, vNormal), 0.0, 1.0)*kd*id;
 
 	// Iluminacion especular de Phong
-	vec3 ks = vec3(0.2, 0.2, 0.2); // Valor constante por ahora, cambiara al definir materiales
-	vec3 is = vec3(0.5, 0.5, 0.5);
-	float glossiness = 200.0;
+	vec3 ks = vec3(1.0, 1.0, 1.0); // Valor constante por ahora, cambiara al definir materiales
+	vec3 is = vec3(0.2, 0.2, 0.2);
+	float glossiness = 300.0;
 	vec3 viewerVector = normalize(uViewerPosition-vPosWorld);
 	vec3 reflectionVector = normalize(2.0*vNormal*dot(lightVec, vNormal) - lightVec);
-	float RdotV = dot(reflectionVector, viewerVector);
+	float RdotV = clamp(dot(reflectionVector, viewerVector), 0.0, 1.0);
 	vec3 specularIllumination = pow(RdotV, glossiness)*ks*is;
 
 	vec3 phongIllumination = ambientIllumination + diffuseIllumination + specularIllumination;
@@ -39,7 +39,7 @@ vec3 phongIlluminationVector() {
 
 void main(void) {
 
-	vec3 colorFinal= mix(uColor, phongIlluminationVector(), 0.2);
+	vec3 colorFinal= uColor * phongReflection();
 
 	gl_FragColor = vec4(colorFinal, 1.0);
 }
